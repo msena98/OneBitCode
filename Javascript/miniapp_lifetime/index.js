77948c6927b4c5d9dcc5edc51148679edcd5d566
matheussena <32408957+msena98@users.noreply.github.com>
2022-05-25 22:22:36 -0300

    Update index.js

--- Javascript/miniapp_lifetime/index.js
+++ Javascript/miniapp_lifetime/index.js
@@ -6,19 +6,19 @@ let today = moment()
 
 let dateDiff = today - departureDate
 
-let chosenOption = prompt("Escolha como gostaria de exibir o tempo de seu nascimento\n1- Segundos\n2- Minutos\n3- Horas\n4- Dias")
+let chosenOption = prompt("Escolha como gostaria de exibir o tempo de vida\n1- Segundos\n2- Minutos\n3- Horas\n4- Dias")
 if(chosenOption == "1") {
     let secondsOfdeparture = Math.round(dateDiff / 1000)
-    alert("Tempo de vôo: " + secondsOfdeparture + " segundos")
+    alert("Tempo de vida: " + secondsOfdeparture + " segundos")
 } else if (chosenOption == "2") {
     let minutesOfdeparture = Math.round(dateDiff / 1000 / 60)
-    alert("Tempo de vôo: " + minutesOfdeparture + " minutos")
+    alert("Tempo de vida: " + minutesOfdeparture + " minutos")
 } else if (chosenOption == "3") {
     let hoursOfdeparture = Math.round(dateDiff / 1000 / 3600)
-    alert("Tempo de vôo: " + hoursOfdeparture + " horas")
+    alert("Tempo de vida: " + hoursOfdeparture + " horas")
 } else if (chosenOption == "4") {
     let daysOfdeparture = Math.round(dateDiff / 1000 / 3600 / 24)
-    alert("Tempo de vôo: " + daysOfdeparture + " dias")
+    alert("Tempo de vida: " + daysOfdeparture + " dias")
 } else {
     alert("Opção inválida!")
 }
